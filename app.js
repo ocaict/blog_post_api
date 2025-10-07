b8d0c56bf64b6d7c9e206908e6817b02a833860e
Oluegwu Chigozie <ocaictcentre@gmail.com>
2025-10-07 21:19:39 +0100

    added cron job

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import postRouter from "./routes/postRoute.js";
+import job from "./utils/cron.js";
+
 const app = express();
 const PORT = process.env.PORT || 3600;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Cron Job
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
 
 // Routes
 app.use(postRouter);
---
+++ utils/cron.js
@@ -0,0 +1,19 @@
+import { CronJob } from "cron";
+
+// Run Cron Job Every 14 Minutes
+const job = new CronJob(
+  "*/14 * * * *", // Runs every 14 minutes
+  async () => {
+    // Task to be executed
+    const baseUrl = process.env.RENDER_URL || "http://localhost:3600";
+    try {
+      const response = await fetch(`${baseUrl}`);
+      const data = await response.json();
+      console.log("Cron Job executed successfully:", data);
+    } catch (error) {
+      console.error("Error executing Cron Job:", error);
+    }
+  }
+);
+
+export default job;
